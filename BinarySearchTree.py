39fa3a2d00f1e7a81f6f513a607c7322c0c2195a
rpc <rpc7863@gmail.com>
2022-07-03 08:58:32 +0530

    added bst and recursion

---
+++ BinarySearchTree.py
@@ -0,0 +1,34 @@
+class BST:
+    def __init__(self, key):
+        self.key = key
+        self.lchild = None
+        self.rchild = None
+
+    def insert(self, data):
+        if self.key is None:
+            self.key = data
+            return
+        if self.key == data:
+            return
+
+        if self.key > data:
+            if self.lchild:
+                self.lchild.insert(data)
+            else:
+                self.lchild = BST(data)
+        else:
+            if self.rchild:
+                self.rchild.insert(data)
+            else:
+                self.rchild = BST(data)
+
+    def __str__(self):
+        return f"{self.key}"
+root = BST(None)
+root.insert(12)
+root.insert(23)
+root.insert(1)
+root.insert(89)
+root.insert(7)
+print(root)
+
