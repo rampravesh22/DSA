92adc739225257f5b0f0d3a72fba03efa00ecaad
rpc <rpc7863@gmail.com>
2022-07-05 10:25:34 +0530

    add

--- Recursion/Merge_sort_recusion.py
+++ Recursion/Merge_sort_recusion.py
@@ -5,6 +5,8 @@ def mergeSort(arr, s, e):
         mergeSort(arr, mid, e)
     merge(arr, s, e)
     print("Hello world")
+    for i in range(10):
+        print("There is no way learning without practicals")
 
 
 mergeSort([2, 5, 1, 3, 0], 0, 4)
