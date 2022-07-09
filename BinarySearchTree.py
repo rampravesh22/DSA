97e958993ac7acff2c11ca312effadc015d6057b
rpc <rpc7863@gmail.com>
2022-07-09 07:20:07 +0530

    new

--- BinarySearchTree.py
+++ BinarySearchTree.py
@@ -1,9 +1,11 @@
 class BST:
+    # for initialization of new node
     def __init__(self, key):
         self.key = key
         self.lchild = None
         self.rchild = None
 
+    # inserting the data in the tree
     def insert(self, data):
         if self.key is None:
             self.key = data
@@ -22,13 +24,35 @@ class BST:
             else:
                 self.rchild = BST(data)
 
+    # Searching data/node present in the tree or not
+    def search(self, data):
+        if self.key == data:
+            print("Node is found at")
+            return
+        if data < self.key:
+            if self.lchild is not None:
+                self.lchild.search(data)
+            else:
+                print("Node is not present in left subtree")
+        else:
+            if self.rchild is not None:
+                self.rchild.search(data)
+            else:
+                print("Node is not present in the right subtree")
+
+    # Traversal operation in tree
+    # 1- Pre order traversal
+    # 2- In order traversal
+    # 3- Post order traversal
+
     def __str__(self):
         return f"{self.key}"
 
 
-root = BST(None)
+root = BST(10)
 l = [23, 4, 67, 1, 90, 2]
-for item in l:
-    root.insert(item)
 
-print(root)
+for data in l:
+    root.insert(data)
+
+root.search(60)
