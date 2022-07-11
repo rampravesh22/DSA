52013dd94ecda6e0b68d5fc680cfc23f6cd3f3bb
rpc <rpc7863@gmail.com>
2022-07-11 13:57:50 +0530

    new

---
+++ GFG/GetMinDifference.py
@@ -0,0 +1,16 @@
+def getMinDiff(arr, n, k):
+        arr.sort()
+        ans, minimum, maximum = arr[n-1] - arr[0], arr[0], arr[n-1]
+        for i in range(1,n):
+            if arr[i] >= k:
+                minimum, maximum = min(arr[0]+k, arr[i]-k), max(arr[i-1]+k, arr[n-1]-k)
+                ans = min(ans, maximum-minimum)
+        return ans
+
+
+k = 5
+n = 10
+input = "2 6 3 4 7 2 10 3 2 1"
+arr = input
+arr=list(map(int,input.split()))
+print(getMinDiff(arr, n, k))
---
+++ Recursion/Arry_Subsequence.py
@@ -0,0 +1,11 @@
+def poswerSet(arr, ind,ans=[]):
+    if ind == len(arr):
+        print(ans,end=" ")
+        return
+    ans.append(arr[ind])
+    poswerSet(arr, ind + 1)
+    ans.remove(arr[ind])
+    poswerSet(arr, ind + 1)
+
+
+poswerSet([3, 1, 2], 0)
