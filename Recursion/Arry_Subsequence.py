07dac8c1d1fd672ca8ca09d0c6dbba354d01c3bb
rpc <rpc7863@gmail.com>
2022-07-17 11:51:06 +0530

    changes in current file

--- Recursion/Arry_Subsequence.py
+++ Recursion/Arry_Subsequence.py
@@ -2,7 +2,7 @@ def poswerSet(arr, ind,ans=[]):
     if ind == len(arr):
         print(ans,end=" ")
         return
-
+    print("hello there how are you")
     # take condition
     ans.append(arr[ind])
     poswerSet(arr, ind + 1)
@@ -10,3 +10,4 @@ def poswerSet(arr, ind,ans=[]):
     # not take condtion
     poswerSet(arr, ind + 1)
 poswerSet([3, 1, 2], 0)
+print("Hide the main structure to event of the javascript that can be useful some time ")
