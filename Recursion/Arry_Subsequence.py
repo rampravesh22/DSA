4a62eedf187092c812b81932d1c6063bb207e771
rpc <rpc7863@gmail.com>
2022-07-17 10:34:04 +0530

    changes in current file

--- Recursion/Arry_Subsequence.py
+++ Recursion/Arry_Subsequence.py
@@ -2,8 +2,11 @@ def poswerSet(arr, ind,ans=[]):
     if ind == len(arr):
         print(ans,end=" ")
         return
+
+    # take condition
     ans.append(arr[ind])
     poswerSet(arr, ind + 1)
     ans.remove(arr[ind])
+    # not take condtion
     poswerSet(arr, ind + 1)
 poswerSet([3, 1, 2], 0)
