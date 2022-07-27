8d36533d58dd6d3108b6e3f289f74c0bf5858f36
rpc <rpc7863@gmail.com>
2022-07-27 14:22:42 +0530

    changes in current file

--- Recursion/combinationSum.py
+++ Recursion/combinationSum.py
@@ -6,15 +6,20 @@ def combinationSum(arr, ind, n, target, ans, l):
     if ind == n:
         if target == 0:
             ans.append(l)
-
+        return
     if arr[ind] <= target:
         l.append(arr[ind])
-        combinationSum(arr, ind + 1, n, target - arr[ind], ans, l)
+        combinationSum(arr, ind, n, target - arr[ind], ans, l)
         l.pop()
     combinationSum(arr, ind + 1, n, target, ans, l)
 
 
 ans = []
 l = []
-combinationSum(arr=[1, 2, 3, 4, 5], ind=0, n=5, target=5, ans=ans, l=l)
+arr = [1, 2, 3, 4, 5]
+ind = 0
+target = 5
+n = len(arr)
+x=combinationSum(arr, ind, n, target, ans, l)
 print(ans)
+print(x)
---
+++ Testing.py
@@ -0,0 +1,20 @@
+def changeme(mylist):
+    mylist.append([1, 2, 3, 4])
+    print("Values inside the function: ", mylist)
+    return
+
+
+mylist = [10, 20, 30]
+changeme(mylist)
+print("Values outside the function: ", mylist)
+
+
+def changeme(mylist):
+    mylist.clear()
+    print("Values inside the function: ", mylist)
+    return
+
+
+mylist = [10, 20, 30]
+changeme(mylist)
+print("Values outside the function: ", mylist)
