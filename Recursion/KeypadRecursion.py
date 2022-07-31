cdad9ae44cd26a71be565b455d4531e85c7687e2
rpc <rpc7863@gmail.com>
2022-07-31 08:50:22 +0530

    changes in current file

--- Recursion/KeypadRecursion.py
+++ Recursion/KeypadRecursion.py
@@ -1,15 +1,30 @@
-def keypadRecursion(digit_str,output_str,index,ans_list,digits_mapping):
+def keypadRecursion(digit_str, output_str, index, ans_list, digits_mapping):
+    if index == len(digits):
+        ans_list.append(output_str)
+        return
+    curr_digit = int(digit_str[index])
+    letter_string_of_digit = digits_mapping[curr_digit]
+    for i in range(len(letter_string_of_digit)):
+        output_str+=letter_string_of_digit[i]
+        keypadRecursion(digit_str,output_str,index+1,ans_list,digits_mapping)
+        output_str=output_str.replace(a)
 
-
-    pass
-    
-
-
-ans=[]
-digits=[]
+ans = []
+digits = "2"
 if len(digits) == 0:
     print(ans)
 output_str = ""
 ind = 0
-digits_mapping =["","",'abc','def','ghi','jkl','mno','pqrs','tuv','wxyz']
-solve(digits,output_str,ind,ans, digits_mapping)
+digits_mapping = {
+    0: "",
+    1: "",
+    2: 'abc',
+    3: 'def',
+    4: 'ghi',
+    5: 'jkl',
+    6: 'mno',
+    7: 'pqrs',
+    8: 'tuv',
+    9: 'wxyz'
+}
+keypadRecursion(digits, output_str, ind, ans, digits_mapping)
