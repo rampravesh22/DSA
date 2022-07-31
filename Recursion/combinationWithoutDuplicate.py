aca32677e4695544089434cab554059121eeb4fd
rpc <rpc7863@gmail.com>
2022-07-31 08:06:24 +0530

    changes in current file

--- Recursion/combinationWithoutDuplicate.py
+++ Recursion/combinationWithoutDuplicate.py
@@ -1,4 +1,22 @@
-def combination(arr, n, ind, target, ds, ans):
-    if ind == n:
-        return "None"
+# Leetcode 40 problem
 
+def combination2(arr, n, ind, target, ds, ans):
+    if target == 0:
+        ans.append(ds.copy())
+        return
+
+    for i in range(ind, n):
+        if i > ind and arr[i] == arr[i - 1]:
+            continue
+        if arr[i] > target:
+            break
+        ds.append(arr[i])
+        combination2(arr,n,i+1, target - arr[i], ds,ans)
+        ds.pop()
+
+
+arr = [1, 1, 1, 2, 2]
+arr.sort()
+ans = []
+combination2(arr,n=len(arr),ind=0,target=4,ds=[],ans=ans)
+print(ans)
--- Testing.py
+++ Testing.py
@@ -1,3 +1,11 @@
-print()
+def is_part(list1, num):
+    s = list(map(str, list1))
+    s="".join(s)
+    num = str(num)
+    if num in s:
+        return True
+    return False
 
-
+list1 = [2,5,3,4]
+n = 5890
+print(is_part(list1,n))
